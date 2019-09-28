a30b4a72fb6ed32e1b07c7da00bd9383c2a19f67
John Ferrell <jdferrell3@gmail.com>
2019-09-27 20:54:03 -0400

    fix cmd/wysparser

--- cmd/wysparser/main.go
+++ cmd/wysparser/main.go
@@ -10,7 +10,8 @@ import (
 
 func main() {
 	args := updater.Args{}
-	wys, err := updater.ParseWYS(os.Args[1], args)
+	info := updater.Info{}
+	wys, err := info.ParseWYS(os.Args[1], args)
 	if nil != err {
 		log.Fatal(err)
 	}
