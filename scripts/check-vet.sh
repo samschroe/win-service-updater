a2e90fd8c099e430a201bbbc503ce57ff972f661
Robert R Meyer <Robert.Meyer@huntresslabs.com>
2023-02-06 16:23:36 -0700

    [SC-77746] Adding scripts for CircleCi

---
+++ scripts/check-vet.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+GOOSES=("darwin" "windows")
+TAGS=("debug" "touchstone")
+
+FAILED=0
+
+function handle_exit {
+    if [[ $1 -eq 0 ]]; then
+        echo -e '\033[0;32mPASSED\033[0m'
+    else
+        FAILED=$1
+        echo -e '\033[0;31mFAILED\033[0m'
+        echo "${@:2}" 
+        echo
+    fi
+}
+
+for OS in "${GOOSES[@]}"; do
+    echo -n "Vetting \"$OS\" without tags: "
+    RESULT=$(GOOS=$OS go vet -tags "full" ./... 2>&1)
+    handle_exit $? $RESULT
+
+    for TAG in "${TAGS[@]}"; do
+        echo -n "Vetting \"$OS\" with \"$TAG\" tag: "
+        RESULT=$(GOOS=$OS go vet -tags "full $TAG" ./... 2>&1)
+        handle_exit $? $RESULT
+    done
+
+done
+
+exit "$FAILED"
